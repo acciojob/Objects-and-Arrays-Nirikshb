56e3dc909deb27bdba5a66a0566d3435e3d50aad
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-01-16 15:27:22 +0530

    update

--- solution.js
+++ solution.js
@@ -9,6 +9,10 @@ const person = {
 var team = players
 var team1 = players.slice()
 
-var cap1 = [...person]
+var cap1 = Object.assign({}, person);
 
+
+// console.log(team);
+// console.log(team1);
+// console.log(cap1);
 export {players,person,team,team1,cap1}
